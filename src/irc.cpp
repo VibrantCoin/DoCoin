f34227e14e4e0f17b45843abdaa825ac574e9e97
Vibrant <suri@datasisar.com>
2016-08-19 11:44:57 +0530

    Changes to DoCoin

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #foocoin2TEST3\r");
-            Send(hSocket, "WHO #foocoin2TEST3\r");
+            Send(hSocket, "JOIN #docoin2TEST3\r");
+            Send(hSocket, "WHO #docoin2TEST3\r");
         } else {
-            // randomly join #foocoin00-#foocoin99
+            // randomly join #docoin00-#docoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #foocoin2%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #foocoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #docoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #docoin2%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
